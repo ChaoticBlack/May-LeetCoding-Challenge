125c0f5b86f99c957e704f5ade430039f53f61b3
Shivam Rajput <55967429+ChaoticBlack@users.noreply.github.com>
2020-05-05 13:59:27 +0530

    Create irst Unique Character in a String.cpp

---
+++ May 5/irst Unique Character in a String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int i; 
+        int j=0;
+        for(i=0;i<s.length();i++)
+        {
+            j=0;
+            while(j<=s.length())
+            {
+                if(s[i]==s[j]&& i!=j)
+                    break;
+                
+               else if(s[i]!=s[j] &&j==s.length())
+                    return i;
+                
+                else
+                    j++;
+                    
+            }
+
+        }
+        return -1;
+    }
+};
